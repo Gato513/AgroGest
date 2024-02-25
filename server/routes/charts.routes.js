1ce0e488a657bea5e23b2664f6264be8333159ab
gato513 <antonioprado5606@gmail.com>
2024-02-25 00:45:02 -0300

    Agragacion de rutas para los nuevos controladores

--- server/routes/charts.routes.js
+++ server/routes/charts.routes.js
@@ -8,7 +8,9 @@ const chartsController = require('../controllers/charts.controller');
 /* Rutas de product */
 router.get("/chart", verifyToken, chartsController.formattedForCharts);
 
-router.get("", verifyToken, chartsController.formattedForCharts);
+router.get("/product", verifyToken, chartsController.DataProductForChart);
+
+router.get("/cattle",  verifyToken, chartsController.DataCattleForChart)
 
 
 module.exports = router;
